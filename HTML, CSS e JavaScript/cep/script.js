c8c2171da926d7b48d973d1bc57e274df6e39cd9
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2023-12-30 16:27:11 -0300

    adicionando aplicação de conversão e formatando outra aplicação

--- HTML, CSS e JavaScript/cep/script.js
+++ HTML, CSS e JavaScript/cep/script.js
@@ -1,10 +1,10 @@
 function formatarCEP(event) {
-    const cep= document.getElementById('cep')
-    const key = event.key
+  const cep = document.getElementById("cep");
+  const key = event.key;
 
-    if (!/\d/.test(key)) event.preventDefault()
-    
-    if (key === 'Backspace') cep.value = null
-    else if (cep.value.length === 2) cep.value += '.'
-    else if (cep.value.length === 6) cep.value += '-'
-  }
+  if (!/\d/.test(key)) event.preventDefault();
+
+  if (key === "Backspace") cep.value = null;
+  else if (cep.value.length === 2) cep.value += ".";
+  else if (cep.value.length === 6) cep.value += "-";
+}
---
+++ HTML, CSS e JavaScript/conversor_unidades/script.js
@@ -0,0 +1,87 @@
+function organizarContainer() {
+  const select = document.getElementById("unidades").value;
+
+  const div_cf = document.getElementById("c_f");
+  const div_kmhms = document.getElementById("kmh_ms");
+
+  if (select === "c_f") {
+    div_cf.style.display = "flex";
+    div_kmhms.style.display = "none";
+  } else if (select === "kmh_ms") {
+    div_cf.style.display = "none";
+    div_kmhms.style.display = "flex";
+  }
+}
+
+function apenasNumerosEPonto(value) {
+  return /^[0-9.]*$/.test(value);
+}
+
+function c_f() {
+  const c = document.getElementById("celsius").value;
+  const f = document.getElementById("fahrenheit");
+
+  if (!apenasNumerosEPonto(c)) {
+    alert("Por favor, insira apenas números e ponto decimal.");
+    return false;
+  }
+
+  const formula = (c * 9) / 5 + 32;
+
+  f.value = formula.toFixed(1);
+
+  if (c == "") {
+    f.value = null;
+  }
+}
+
+function f_c() {
+  const c = document.getElementById("celsius");
+  const f = document.getElementById("fahrenheit").value;
+
+  const formula = ((f - 32) * 5) / 9;
+
+  if (!apenasNumerosEPonto(f)) {
+    alert("Por favor, insira apenas números e ponto decimal.");
+  }
+
+  c.value = formula.toFixed(1);
+
+  if (f == "") {
+    c.value = null;
+  }
+}
+
+function kmh_ms() {
+  const kmh = document.getElementById("kmh").value;
+  const ms = document.getElementById("ms");
+
+  if (!apenasNumerosEPonto(kmh)) {
+    alert("Por favor, insira apenas números e ponto decimal.");
+  }
+
+  const formula = kmh / 3.6;
+
+  ms.value = formula.toFixed(2);
+
+  if (kmh == "") {
+    ms.value = null;
+  }
+}
+
+function ms_kmh() {
+  const ms = document.getElementById("ms").value;
+  const kmh = document.getElementById("kmh");
+
+  if (!apenasNumerosEPonto(kmh)) {
+    alert("Por favor, insira apenas números e ponto decimal.");
+  }
+
+  const formula = ms * 3.6;
+
+  kmh.value = formula.toFixed(2);
+
+  if (ms == "") {
+    kmh.value = null;
+  }
+}
